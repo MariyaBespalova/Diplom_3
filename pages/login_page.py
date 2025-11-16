f20f713168524ac4bb2e8fe4dd371d45060a01bc
MariyaBespalova <bespalovamaschka@yandex.ru>
2025-11-16 19:11:08 +0300

    Update login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,5 +1,4 @@
 import allure
-from pages.order_feed_page import OrderFeedPage
 from pages.base_page import BasePage
 from pages.main_page import MainPage
 from locators.login_page_locators import LoginPageLocators
@@ -14,12 +13,9 @@ class LoginPage(BasePage):
         self.set_text_to_element(LoginPageLocators.INPUT_EMAIL, email)
         self.set_text_to_element(LoginPageLocators.INPUT_PASSWORD, password)
         self.click_to_element(LoginPageLocators.BUTTON_LOGIN)
-        return MainPage(self.driver)
-    
+        
+        
     
     @allure.step('Проверка, что страница открылась')
-    def _verify_page_loaded(self):
-        conditions = [
-            self.find_visible_element(LoginPageLocators.H2_LOGIN_TITLE)
-        ]
-        return all(conditions)
+    def is_loaded(self):
+        return self._verify_page_loaded(LoginPageLocators.H2_LOGIN_TITLE)
