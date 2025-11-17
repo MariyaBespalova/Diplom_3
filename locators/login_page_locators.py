3169ab18d6164e3bf0e77a4708f93772ef191ec2
MariyaBespalova <bespalovamaschka@yandex.ru>
2025-11-17 06:50:21 +0300

    Update login_page_locators.py

--- locators/login_page_locators.py
+++ locators/login_page_locators.py
@@ -2,7 +2,7 @@ from selenium.webdriver.common.by import By
 
 
 class LoginPageLocators:
-    H2_LOGIN_TITLE = By.XPATH, ".//h2[text()='Вход']"
-    BUTTON_LOGIN = By.XPATH, ".//button[text()='Войти']"
-    INPUT_EMAIL= By.XPATH, ".//input[@name='name']"
-    INPUT_PASSWORD = By.XPATH, ".//input[@name='Пароль']"
+    H2_LOGIN_TITLE = By.XPATH, "//h2[text()='Вход']"
+    BUTTON_LOGIN = By.XPATH, "//button[text()='Войти']"
+    INPUT_EMAIL = By.XPATH, "//input[@name='name']"
+    INPUT_PASSWORD = By.XPATH, "//input[@name='Пароль']"
