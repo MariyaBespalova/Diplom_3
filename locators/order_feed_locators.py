0e0915c153282269b5f4884919e7d18d1e8ed3ff
MariyaBespalova <bespalovamaschka@yandex.ru>
2025-11-17 06:51:28 +0300

    Update order_feed_locators.py

--- locators/order_feed_locators.py
+++ locators/order_feed_locators.py
@@ -1,12 +1,9 @@
 from selenium.webdriver.common.by import By
 
-
 class OrderFeedLocators:
-    LINK_ORDER_FEED_ACTIVE = By.XPATH, ".//a[@aria-current='page']/p[text()='Лента Заказов']"
-    LINK_ORDERS = By.XPATH, "(.//a[contains(@class, 'OrderHistory_link')])"
-    SECTION_ORDER_DETAILS = By.XPATH, ".//section[contains(@class, 'Modal_modal_opened')]"
-    P_ORDERS_GLOBAL_COUNTER = By.XPATH, ".//p[text()='Выполнено за все время:']/following-sibling::p"   
-    P_ORDERS_TODAY_COUNTER = By.XPATH, ".//p[text()='Выполнено за сегодня:']/following-sibling::p"
-    LI_ORDERS_IN_PROGRESS = By.XPATH, "(.//ul[contains(@class, 'OrderFeed_orderListReady')]/li)[1]"
-    ORDERS_FEED_LINK = By.XPATH, "(.//*[@id='root']/div/header/nav/ul/li[2]/a/p)"
-    UNIQUE_ELEMENT_LOCATOR = By.XPATH, "//h1[contains(text(), 'Лента заказов')]"
+    LINK_ORDER_FEED_ACTIVE = By.XPATH, "//a[contains(@class, 'AppHeader_header__link_active')]/p[text()='Лента Заказов']"
+    LINK_ORDERS = By.XPATH, "//a[contains(@class, 'OrderHistory_link')]"
+    SECTION_ORDER_DETAILS = By.XPATH, "//section[contains(@class, 'Modal_modal') and contains(@class, 'Modal_modal_opened')]"
+    P_ORDERS_GLOBAL_COUNTER = By.XPATH, "//p[text()='Выполнено за все время:']/following-sibling::p[contains(@class, 'OrderFeed_number')]"   
+    P_ORDERS_TODAY_COUNTER = By.XPATH, "//p[text()='Выполнено за сегодня:']/following-sibling::p[contains(@class, 'OrderFeed_number')]"
+    LI_ORDERS_IN_PROGRESS = By.XPATH, "(//ul[contains(@class, 'OrderFeed_orderListReady')]//li)[1]"
